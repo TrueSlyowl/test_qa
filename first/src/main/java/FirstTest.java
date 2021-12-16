c8ab445fe62b004ad88054816b417664468fcef4
TrueSlyowl <95303264+TrueSlyowl@users.noreply.github.com>
2021-12-16 21:34:30 +0300

    типы данных

--- first/src/main/java/FirstTest.java
+++ first/src/main/java/FirstTest.java
@@ -2,5 +2,8 @@ public class FirstTest {
 	
 	public static void main (String[] args) {
 		System.out.println ("We are here! Hello! ");
+		int s = 5;
+		System.out.println ("Какой то текст "+s*s*s);
+
 	}
 }
