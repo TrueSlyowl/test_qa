f6353cd93803ab2590494d3bcd6c43640e7c0c6c
TrueSlyowl <95303264+TrueSlyowl@users.noreply.github.com>
2021-12-16 21:51:08 +0300

    методы

--- first/src/main/java/FirstTest.java
+++ first/src/main/java/FirstTest.java
@@ -1,9 +1,27 @@
 public class FirstTest {
 	
 	public static void main (String[] args) {
-		System.out.println ("We are here! Hello! ");
-		int s = 5;
-		System.out.println ("Какой то текст "+s*s*s);
+		helloTest();
+		helloTest2( "для примера");
+		System.out.println(helloTest3(3,5));
+	}
+	public static void helloTest ()
+		{
+			System.out.println("Текст");
+
+		}
+
+
+	public static void helloTest2(String text)
+	{
+		System.out.println("Текст " + text);
 
 	}
+	public static String helloTest3(int value1, int value2)
+	{
+		return "Числа для примера "+value1+"+"+value2;
+	}
+
+
+
 }
